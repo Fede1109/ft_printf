230c1f7234f3019a7bf86ed52f59f021b7edaeef
Federico Jose Diaz Guanoluiza <fdiaz-gu@c2r10s1.42madrid.com>
2023-10-02 13:58:09 +0200

    print_p added

--- ft_print_p.c
+++ ft_print_p.c
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 15:48:46 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/09/28 16:41:18 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/09/29 15:48:45 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ int	ft_print_p(void *arg)
 	alpha = "0123456789abcdef";
 	count = 0;
 	count += ft_print_string("0x");
+	if (count != 2)
+		return (-1);
 	count += ft_print_x(arg2, alpha);
 	return (count);
 }
