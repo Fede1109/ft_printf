2b1c541f5293c307334f61b43f433a8a95143dba
Federico Jose Diaz Guanoluiza <fdiaz-gu@c2r10s1.42madrid.com>
2023-10-02 13:57:22 +0200

    print_unsigned added

--- ft_print_unsigned.c
+++ ft_print_unsigned.c
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 15:34:08 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/09/28 16:12:36 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/09/29 16:49:55 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,15 @@ int	ft_print_unsigned(unsigned int nb)
 	if (nb > 9)
 	{
 		count += ft_print_d_i(nb / 10);
+		if (count < 0)
+			return (-1);
 		count += ft_print_d_i(nb % 10);
 	}
 	else if (nb >= 0 && nb <= 9)
+	{
 		count += ft_print_char(nb + '0');
-	if (count < 0)
+	}
+	else
 		return (-1);
 	return (count);
 }
