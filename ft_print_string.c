4234c55c7555d6f3b41f459d22507c017b99428e
Federico Jose Diaz Guanoluiza <fdiaz-gu@c2r10s1.42madrid.com>
2023-10-02 13:57:46 +0200

    print string added

--- ft_print_string.c
+++ ft_print_string.c
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 15:33:44 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/09/28 16:12:57 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/09/29 15:47:12 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,19 @@
 int	ft_print_string(char *str)
 {
 	int	count;
+	int	flag;
 
+	flag = 0;
 	count = 0;
 	if (!str)
-		return (write(1, "(null)", 6));
+		return (ft_print_string("(null)"));
 	while (str[count])
 	{
-		write(1, &str[count], 1);
+		if (ft_print_char(str[count]) == -1)
+			return (-1);
 		count++;
 	}
+	if (count < 0)
+		return (-1);
 	return (count);
 }
