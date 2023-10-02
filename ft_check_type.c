39f1a2a9023800e7b01e6aff99f176ee75fa7551
Federico Jose Diaz Guanoluiza <fdiaz-gu@c2r10s1.42madrid.com>
2023-10-02 14:00:33 +0200

    ft_check

--- ft_check_type.c
+++ ft_check_type.c
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 15:41:09 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/09/28 16:41:06 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/09/29 15:34:30 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,18 @@ int	ft_check_type(const char type, va_list args)
 
 	count = 0;
 	if (type == 'c')
-		count += (char)ft_print_char(va_arg(args, int));
+		count += ft_print_char((char)va_arg(args, int));
 	if (type == 's')
-		count += ft_print_string(va_arg(args, char *));
+		count += ft_print_string((char *)va_arg(args, char *));
 	if (type == 'd' || type == 'i')
-		count += ft_print_d_i(va_arg(args, int));
+		count += ft_print_d_i((int)va_arg(args, int));
 	if (type == 'p')
 		count += ft_print_p(va_arg(args, void *));
 	if (type == 'u')
-		count += ft_print_unsigned(va_arg(args, unsigned int));
+		count += ft_print_unsigned((unsigned int)va_arg(args, unsigned int));
 	if (type == 'X' || type == 'x')
-		count += ft_set_x_type(va_arg(args, int), type);
+		count += ft_set_x_type((int)va_arg(args, int), type);
 	if (type == '%')
-		return (write(1, "%", 1));
+		count += ft_print_char('%');
 	return (count);
 }
